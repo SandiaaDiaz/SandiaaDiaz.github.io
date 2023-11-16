8d2b477656c58f769a3071bef54061f77e4ee47e
Sandra Diaz <132292409+SandiaaDiaz@users.noreply.github.com>
2023-11-15 19:24:32 -0600

    Add files via upload
    Second

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
+
 });
  
 typewriter
@@ -11,3 +12,4 @@ typewriter
   .pauseFor(200)
   .deleteChars(10)
   .start();
+  
